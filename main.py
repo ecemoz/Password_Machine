ad6eef0743ed0f6fc7d0f04d0869cb61dc06916f
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-14 23:28:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+BG_COLOR ="#17141F"
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website}| {email} | {password}\n")
+        website_entry.delete(0,END)
+        password_entry.delete(0,END)
+        email_entry.delete(0,END)
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx= 50, pady= 50, bg= BG_COLOR)
+
+canvas = Canvas(height= 200, width= 200,bg= BG_COLOR, highlightthickness= 0)
+logo_img = PhotoImage(file= "image.png")
+canvas.create_image(100,100, image= logo_img)
+canvas.grid(row= 0, column= 1)
+
+website_label = Label(text="Website:", bg= BG_COLOR, fg="white")
+website_label.grid(row= 1, column= 0)
+email_label = Label(text="Email/Username:", bg= BG_COLOR, fg="white")
+email_label.grid(row= 2, column= 0)
+password_label = Label(text="Password:", bg= BG_COLOR, fg="white")
+password_label.grid(row= 3, column= 0)
+
+website_entry = Entry(width= 35)
+website_entry.grid(row= 1, column= 1, columnspan= 2)
+website_entry.focus()
+email_entry = Entry(width= 35)
+email_entry.grid(row= 2, column= 1, columnspan= 2)
+password_entry = Entry(width= 21)
+password_entry.grid(row= 3, column= 1)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column= 2)
+add_button = Button(text="Add",width= 28, command=save )
+add_button.grid(row= 4, column= 1, columnspan= 2)
+
+
+
+
+
+
+
+window.mainloop()
